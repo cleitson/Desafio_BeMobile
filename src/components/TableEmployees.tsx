614a367d519be65dc47275c4b858faf264807a58
cleitson <cleitson.ftw@gmail.com>
2025-02-26 10:23:26 -0300

    tabela com filtro ativo

--- src/components/TableEmployees.tsx
+++ src/components/TableEmployees.tsx
@@ -3,44 +3,55 @@ import Context from "../Context/Context";
 import { ConvertDate, formatPhoneNumber } from "../utils/Formatters";
 
 function TableEmployees() {
-  const { employees } = useContext(Context);
+  const { filteredEmployees, loading } = useContext(Context);
 
-  // Early return for invalid or empty data
-  if (!Array.isArray(employees) || employees.length === 0) {
-    return <h1 className="text-center">No employees to display.</h1>;
+  if (loading) {
+    return (
+      <div className="w-full max-w-11/12 mx-auto justify-center items-center flex">
+        <h1>Loading...</h1>
+      </div>
+    );
   }
 
   return (
-    <table className="w-full max-w-11/12 mx-auto">
-      <thead className="bg-blue-primary text-white">
-        <tr>
-          <th className="p-3 text-center font-bold rounded-tl-lg">FOTO</th>
-          <th className="p-3 text-center font-bold">NOME</th>
-          <th className="p-3 text-center font-bold">CARGO</th>
-          <th className="p-3 text-center font-bold">DATA DE ADMISSÃO</th>
-          <th className="p-3 text-center font-bold rounded-tr-lg">TELEFONE</th>
-        </tr>
-      </thead>
-      <tbody className="bg-white">
-        {employees.map((employee) => (
-          <tr key={employee.id} className="shadow-01">
-            <td className="p-3 text-center">
-              <img
-                src={employee.image}
-                alt={employee.name}
-                width={34}
-                height={34}
-                className="rounded-full mx-auto"
-              />
-            </td>
-            <td className="p-3 text-center">{employee.name}</td>
-            <td className="p-3 text-center">{employee.job}</td>
-            <td className="p-3 text-center">{ConvertDate(employee.admission_date)}</td>
-            <td className="p-3 text-center">{formatPhoneNumber(employee.phone)}</td>
+    <>
+      <table className="w-full max-w-11/12 mx-auto">
+        <thead className="bg-blue-primary text-white">
+          <tr>
+            <th className="p-3 text-center font-bold rounded-tl-lg">FOTO</th>
+            <th className="p-3 text-center font-bold">NOME</th>
+            <th className="p-3 text-center font-bold">CARGO</th>
+            <th className="p-3 text-center font-bold">DATA DE ADMISSÃO</th>
+            <th className="p-3 text-center font-bold rounded-tr-lg">TELEFONE</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="bg-white p-3 text-center">
+          {filteredEmployees.length > 0 && filteredEmployees.map((employee) => (
+            <tr key={employee.id} className="shadow-01">
+              <td className="p-3 text-center">
+                <img
+                  src={employee.image}
+                  alt={employee.name}
+                  width={34}
+                  height={34}
+                  className="rounded-full mx-auto"
+                />
+              </td>
+              <td>{employee.name}</td>
+              <td>{employee.job}</td>
+              <td>{ConvertDate(employee.admission_date)}</td>
+              <td>{formatPhoneNumber(employee.phone)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {
+        filteredEmployees.length === 0 &&
+        <div className="w-full max-w-11/12 mx-auto justify-center items-center flex">
+          <h1>No employees to display.</h1>
+        </div>
+      }
+    </>
   );
 }
 
